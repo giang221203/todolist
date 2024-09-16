d069826f8d515dd418161baa1fd3b2c02bad4ef7
unknown <nguyengiang22122003@gmail.com>
2024-09-16 16:34:00 +0700

    update

--- font/src/app/app-routing.module.ts
+++ font/src/app/app-routing.module.ts
@@ -12,7 +12,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: false,
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
